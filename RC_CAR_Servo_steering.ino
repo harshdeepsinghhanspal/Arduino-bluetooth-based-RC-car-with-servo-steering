5a8f02e5637dffd92b8d35fe2ac36c7923513327
Harshdeep Singh <96439130+harshdeepsinghhanspal@users.noreply.github.com>
2022-09-17 10:49:35 +0530

    Update RC_CAR_Servo_steering.ino

--- RC_CAR_Servo_steering.ino
+++ RC_CAR_Servo_steering.ino
@@ -1,100 +1,128 @@
+//This program is made to operate with "Bluetooth RC Controller" app
+//Available on the playstore(Android) and appstore(iOS)
+//----------------------------------------------------------------------
+
 #include <AFMotor.h>
 #include <Servo.h>
 
+//HCSR04
+#define echo A5 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trig 2 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+//Servo motor
 Servo myServo1;
 Servo myServo2;
 
+//DC Motor
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(4);
 
 int angle=90;       //Servo will stay at the center by default (90 is the center of 0 and 180)
-int angleStep=30;   //This will be reduced from "angle" and the value obtained is the desired value
+int angleStep=30;   //This will be reduced from "angle" and the value obtained is the desired value for turning the video
 
-char bt='S';
+int minDist = 10;        //Collision avoidance
+int maxDist = 4000;        //Collision avoidance
+
+//Bluetooth part
+char bt='S';        //To keep the car at stop by default, S in the app means STOP
+
+//Lights
+int Headlight = 5;        //These digital pins are not at all connected to the L293D module so can be used
+int Taillight = 13;
+
+//Horn
+int Honky = 3;      //Connected to the M2 which is connected to the digital pin 3
+
 void setup()
 {
   Serial.begin(9600);
-
   myServo1.attach(9);
   myServo2.attach(10);
 
   myServo1.write(angle);
   myServo2.write(angle);
+
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
   
   motor1.setSpeed(255);
   motor2.setSpeed(255);
   Stop();
+
+  pinMode(Headlight, OUTPUT);
+  pinMode(Taillight, OUTPUT);
+
+  pinMode(Honky, OUTPUT);
 }
 
 
 void loop() {
- 
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(2);
+  long t = pulseIn(echo,HIGH);
+  long cm = t / 29 / 2;
+
+  Serial.println(cm);
+  
   bt=Serial.read();
+  switch(bt){
   
   //Straight movement
-  if(bt=='F')
-  {
-   forward();
-   myServo1.write(angle); 
-  }
-  if(bt=='B')
-  {
-   backward();
-   myServo1.write(angle); 
-  }
+  case 'F': if(cm>minDist && cm<maxDist){forward(); myServo1.write(angle);} 
+            else{Stop();} break;
+            
+  case 'B': backward(); myServo1.write(angle); break;  
 
   //Forward directional movement
-  if(bt=='G')
-  {
-   left();
-   forward();
-  }
-  if(bt=='I')
-  {
-   right();
-   forward();
-  }
+  case 'G': left(); forward(); break;
+  case 'I': right(); forward(); break;
   
   //Backward directional movement
-  if(bt=='H')
-  {
-   left();
-   backward(); 
-  }
-  if(bt=='J')
-  {
-   right(); 
-   backward();
-  }
+  case 'H': left(); backward(); break;
+  case 'J': right(); backward(); break;
   
   //Steering only movement
-  if(bt=='L')
-  {
-   left();
-  }
-  if(bt=='R')
-  {
-   right(); 
-  }
-
+  case 'L': left(); break;
+  case 'R': right(); break; 
+ 
   //Door ops
-  if(bt=='X')
-  {
-   doorOpen(); 
-  }
-
-  if(bt=='x')
-  {
-   doorClose(); 
-  }
+  case 'X': doorOpen(); break; 
+  case 'x': doorClose(); break;
   
   //Stop the motors!!!
-  if(bt=='S')
-  {
-   Stop(); 
+  case 'S': Stop(); break;
+
+  //Motor speed
+  case '0': motor1.setSpeed(0); motor2.setSpeed(0); break;
+  case '1': motor1.setSpeed(25); motor2.setSpeed(25); break;
+  case '2': motor1.setSpeed(50); motor2.setSpeed(50); break;
+  case '3': motor1.setSpeed(75); motor2.setSpeed(75); break;
+  case '4': motor1.setSpeed(100); motor2.setSpeed(100); break;
+  case '5': motor1.setSpeed(125); motor2.setSpeed(125); break;
+  case '6': motor1.setSpeed(150); motor2.setSpeed(150); break;
+  case '7': motor1.setSpeed(175); motor2.setSpeed(175); break;
+  case '8': motor1.setSpeed(200); motor2.setSpeed(200); break;
+  case '9': motor1.setSpeed(225); motor2.setSpeed(225); break;
+  case 'q': motor1.setSpeed(255); motor2.setSpeed(255); break;
+
+  //Front Lights control
+  case 'W': digitalWrite(Headlight, HIGH); break;
+  case 'w': digitalWrite(Headlight, LOW); break;
+
+  //Back Lights control
+  case 'U': digitalWrite(Taillight,HIGH); break;
+  case 'u': digitalWrite(Taillight,LOW); break;
+
+  //Horn Honky
+  case 'V': digitalWrite(Honky,HIGH); break;
+  case 'v': digitalWrite(Honky,LOW); break;
   }
 }
 
+
+//------FUNCTIONS------
 //Straight movement
 void forward()
 {
@@ -120,7 +148,7 @@ void right(){
 
 //Door ops
 void doorOpen(){
-  myServo2.write(angle-angleStep);
+  myServo2.write(angle-(2*angleStep));
 }
 
 void doorClose(){
